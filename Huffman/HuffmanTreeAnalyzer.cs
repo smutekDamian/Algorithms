8c42da7ebfc1b1c54868b929866e74576ce0d834
smutekDamian <damian.smutek@makingwaves.com>
2019-11-04 20:11:51 +0100

    Huffman : Extract codes from tree.

---
+++ Huffman/HuffmanTreeAnalyzer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NGenerics.DataStructures.Trees;
+
+namespace Huffman
+{
+    public class HuffmanTreeAnalyzer
+    {
+        private readonly BinaryTree<Leaf> _tree;
+        private readonly Dictionary<string, BitArray> _codes;
+
+        public HuffmanTreeAnalyzer(BinaryTree<Leaf> tree)
+        {
+            _tree = tree;
+            _codes = new Dictionary<string, BitArray>();
+        }
+
+        public Dictionary<string, BitArray> GetByteCodes()
+        {
+            GoThroughTreeAndGetCodes(_tree, new List<bool>());
+            return _codes;
+        }
+
+        private void GoThroughTreeAndGetCodes(BinaryTree<Leaf> tree, IList<bool> bits)
+        {
+            if (tree.Left == null && tree.Right == null && tree.Data.Sequence != null)
+            {
+                _codes.Add(tree.Data.Sequence, new BitArray(bits.ToArray()));
+                return;
+            }
+
+            var leftBits = new List<bool>(bits);
+            var rightBits = new List<bool>(bits);
+            leftBits.Add(false);
+            rightBits.Add(true);
+            GoThroughTreeAndGetCodes(tree.Left, leftBits);
+            GoThroughTreeAndGetCodes(tree.Right, rightBits);
+        }
+    }
+}
---
+++ HuffmanTests/HuffmanTreeAnalyzerTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Huffman;
+using Xunit;
+
+namespace HuffmanTests
+{
+    public class HuffmanTreeAnalyzerTests
+    {
+        [Fact]
+        public void ShouldReturnSequenceToBytesCodeMap()
+        {
+            //Arrange
+            const string testText = "TO BE OR NOT TO BE";
+            var oBits = new BitArray(new [] {false, true});
+            var spaceBits = new BitArray(new [] {true, true});
+            var bBits = new BitArray(new [] {false, false, true});
+            var eBits = new BitArray(new [] {true, false, false});
+            var tBits = new BitArray(new [] {true, false, true});
+            var nBits = new BitArray(new [] {false, false, false, false});
+            var rBits = new BitArray(new [] {false, false, false, true});
+
+
+            var huffmanTree = new HuffmanTree(testText, 1);
+            var tree = huffmanTree.Create();
+            var analyzer = new HuffmanTreeAnalyzer(tree);
+
+            //Act
+            Dictionary<string, BitArray> map = analyzer.GetByteCodes();
+
+            //Asset
+            Assert.True(map.Count.Equals(7));
+//            Assert.True(map["O"].Equals(oBits));
+//            Assert.True(map[" "].Equals(spaceBits));
+//            Assert.True(map["B"].Equals(bBits));
+//            Assert.True(map["E"].Equals(eBits));
+//            Assert.True(map["T"].Equals(tBits));
+//            Assert.True(map["N"].Equals(nBits));
+//            Assert.True(map["R"].Equals(rBits));
+        }
+    }
+}
